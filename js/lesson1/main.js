1469fd71e9103740fbcabd121c99cc9bcce4cfc4
at77777 <vxjhxz5cfuqn6jp7xwrslxtk@gmail.com>
2022-11-01 17:49:57 +0200

    add lesson 3

---
+++ js/lesson1/main.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// 1
+let hi = 'hello';
+let school = 'owu';
+let domen1 = 'com';
+let domen2 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let pi = 3.14;
+let num5 = 2.7;
+let num6 = 16;
+let bool1 = true;
+let bool2 = false;
+
+console.log(hi);
+console.log(school);
+console.log(domen1);
+console.log(domen2);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(pi);
+console.log(num5);
+console.log(num6);
+console.log(bool1);
+console.log(bool2);
+
+
+// 2
+let firstName = 'Andrii';
+let middleName = 'Andriiovych';
+let lastName = 'Tkach';
+
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+
+
+// 3
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// additional
+let firstName1 = prompt('Fisrt Name:');
+let middleName1 = prompt('Middle Name:');
+let lastName1 = prompt('Last Name:');
+let age = prompt('Age:');
+
+console.log(firstName1);
+console.log(middleName1);
+console.log(lastName1);
+console.log(age);
+
