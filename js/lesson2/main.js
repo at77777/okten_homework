17b71c03a27b620c7b8d79e92feee97fb152b1d7
at77777 <vxjhxz5cfuqn6jp7xwrslxtk@gmail.com>
2022-11-03 18:00:14 +0200

    add lesson 4

---
+++ js/lesson2/main.js
@@ -0,0 +1,154 @@
+"use strict";
+
+// 1
+let arr = ['Ferrari', 'Jaguar', 'Bentley', 'Maybach', 'Maserati', 'Porsche', 'Lamborghini', 'Aston Martin', 'Bugatti', 'Tesla'];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+// 2
+
+let book1 = {
+    title: '451° Fahrenheit',
+    pageCount: 208,
+    genre: 'science fiction',
+};
+
+let book2 = {
+    title: 'A Tree Grows in Brooklyn',
+    pageCount: 448,
+    genre: 'novel',
+};
+
+let book3 = {
+    title: 'Birdy',
+    pageCount: 384,
+    genre: 'novel, fiction',
+};
+
+// 3
+let book4 = {
+    title: 'The Handmaids Tale',
+    pageCount: 384,
+    genre: 'novel',
+    authors: [{name: 'Margaret Atwood'}, {age:82}]
+};
+
+let book5 = {
+    title: 'Roots: The Saga of an American Family',
+    pageCount: 736,
+    genre: 'biography',
+    authors: [{name: 'Alex Haley'}, {age:90}]
+};
+
+let book6 = {
+    title: 'When Nietzsche Wept',
+    pageCount: 486,
+    genre: 'fiction',
+    authors: [{name: 'Irvin Yalom'}, {age:91}]
+};
+
+let book7 = {
+    title: 'The Book Thief',
+    pageCount: 608,
+    genre: 'novel',
+    authors: [{name: 'Markus Zusak'}, {age:47}]
+};
+
+let book8 = {
+    title: 'Extremely Loud and Incredibly Close',
+    pageCount: 416,
+    genre: 'novel',
+    authors: [{name: 'Jonathan Safran Foer'}, {age:45}]
+};
+
+let book9 = {
+    title: 'Flowers for Algernon',
+    pageCount: 320,
+    genre: 'science fiction',
+    authors: [{name: 'Daniel Keyes'}, {age:94}]
+};
+
+// 4
+let user1 = {
+    name: 'Andrew',
+    username: 'yawTEjjApZ',
+    password: 'g7iL88!5Qkj0',
+};
+
+let user2= {
+    name: 'Ivan',
+    username: 'OMAFXAAJLI',
+    password: '63x9jHNgOzC%',
+};
+
+let user3 = {
+    name: 'Alex',
+    username: 'pnVZnCNVsX',
+    password: 'zsCtwU&581H8',
+};
+
+let user4 = {
+    name: 'Helen',
+    username: 'Mfjiwpmkgr',
+    password: 'd8rL%1v#3Ud6',
+};
+
+let user5 = {
+    name: 'Alina',
+    username: 'hLsMPXytcU',
+    password: '2%086894LOHo',
+};
+
+let user6 = {
+    name: 'Oleg',
+    username: 'UhwDWdBszS',
+    password: '74%L&ZUnkNW6',
+};
+
+let user7 = {
+    name: 'Svitlana',
+    username: 'EdOVhNCQvK',
+    password: '74%L&ZUnkNW6',
+};
+
+let user8 = {
+    name: 'Bogdan',
+    username: 'NFQinwYmdL',
+    password: 'w06&b7#&NBsY',
+};
+
+let user9 = {
+    name: 'Yevgen',
+    username: 'EOZMjJGLcX',
+    password: '88jx%1MFwuXd',
+};
+
+let user10 = {
+    name: 'Ivana',
+    username: 'xovhezsCth',
+    password: '167Kbm4e^I#G',
+};
+
+let users = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10];
+
+console.log(users[0]);
+console.log(users[1]);
+console.log(users[2]);
+console.log(users[3]);
+console.log(users[4]);
+console.log(users[5]);
+console.log(users[6]);
+console.log(users[7]);
+console.log(users[8]);
+console.log(users[9]);
+
+
