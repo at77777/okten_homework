1cdba7ef9ec30c90ec5c7dba16f86b407153b1f4
at77777 <vxjhxz5cfuqn6jp7xwrslxtk@gmail.com>
2023-01-05 18:02:05 +0200

    mini project upd

--- js/mini-project/main.js
+++ js/mini-project/main.js
@@ -2,14 +2,14 @@ let container = document.createElement('div');
 document.body.append(container);
 
 fetch ('https://jsonplaceholder.typicode.com/users')
-    .then((response)=> response.json())
+    .then((data) => data.json())
     .then(users => {
         for (const user of users) {
             let div = document.createElement('div');
             div.innerText = `${user.id} ${user.name}`;
             let a = document.createElement('a');
             a.innerText = `More`;
-            a.href = `user-details.html`;
+            a.href = `user-details.html?id=${user.id}`;
             div.appendChild(a);
             container.appendChild(div);
         }
