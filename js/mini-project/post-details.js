604eb90bf16fee75da14a4661660cff8f64b4668
at77777 <vxjhxz5cfuqn6jp7xwrslxtk@gmail.com>
2023-01-10 18:00:14 +0200

    mini project UPD

--- js/mini-project/post-details.js
+++ js/mini-project/post-details.js
@@ -1,15 +1,15 @@
 let container = document.createElement('div');
+document.body.append(container);
 
 let url = new URL(location.href);
 let post = url.searchParams.get('post');
 let parse = JSON.parse(post);
 
+let div = document.createElement('div');
+container.appendChild(div);
+
 for (const element in parse) {
-    let ul = document.createElement('ul');
-    for (const key in element) {
-        let li = document.createElement('li');
-        li.innerText = `${key}: ${element[key]}`;
-        ul.appendChild(li);
-    }
-container.appendChild(ul);
+    let innerDiv = document.createElement('div');
+    innerDiv.innerText = `${element}: ${parse[element]}`;
+    div.appendChild(innerDiv);
 }
